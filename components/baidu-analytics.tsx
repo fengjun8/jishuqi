c87539a05f2e5b025c48a63b23e068ea6c416909
fengjun <fengjun@nova.net.cn>
2025-05-28 17:46:38 +0800

    修改百度统计代码

--- components/baidu-analytics.tsx
+++ components/baidu-analytics.tsx
@@ -7,7 +7,7 @@ export default function BaiduAnalytics() {
     // 百度统计代码
     window._hmt = window._hmt || []
     const hm = document.createElement("script")
-    hm.src = "https://hm.baidu.com/hm.js?1ed1451775a140fc3f977bec4298364e" // 替换为您的百度统计跟踪ID
+    hm.src = "https://hm.baidu.com/hm.js?fe9440bb9ea47ce70692e6675ab4fad5"
     const s = document.getElementsByTagName("script")[0]
     s.parentNode?.insertBefore(hm, s)
   }, [])
